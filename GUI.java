322135f79dec4db1db0a3ebd043ac3102bbfb529
AgentALM <67155938+AgentALM@users.noreply.github.com>
2021-03-23 19:26:49 -0700

    Create GUI.java

---
+++ GUI.java
@@ -0,0 +1,45 @@
+package me.Sabu.trailsgui.models;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class GUI {
+
+	private static Inventory INV;
+	
+	public void register() {
+		Inventory inv = Bukkit.createInventory(null, 9, ChatColor.AQUA + "" + ChatColor.BOLD + "trails GUI");
+		
+		ItemStack item = new  ItemStack(Material.TOTEM_OF_UNDYING);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.YELLOW + "Totem trail");
+		item.setItemMeta(meta);
+		inv.setItem(3, item);
+		
+		item = new  ItemStack(Material.CAMPFIRE);
+		meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.RED + "Fire trail");
+		item.setItemMeta(meta);
+		inv.setItem(5, item);
+		
+		setInventory(inv);
+	}
+	
+	public Inventory getInventory() {
+		return INV;
+	}
+	private void setInventory(Inventory inv) {
+		INV = inv;
+	}
+	
+	public void openInventory(Player player) {
+		player.openInventory(INV);
+	}
+	
+}
